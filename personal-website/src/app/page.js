4958dd621f55fef7b4ead08fd24db3a95ea386d5
Christian Zintel <c.zintel@yahoo.de>
2023-05-28 17:33:30 +0200

    creation of placeholder text, added basic structure

--- personal-website/src/app/page.js
+++ personal-website/src/app/page.js
@@ -10,7 +10,85 @@ export default function Home() {
 				<div className="basis-4/12 bg-slate-500 rounded-md h-[107.5%] self-center">
 					Picture
 				</div>
-				<div className="basis-7/12  rounded-r-md overflow-clip">Bio</div>
+				<div className="basis-7/12 rounded-r-md overflow-scroll p-8">
+					<div className="flex flex-col gap-y-8">
+						<div className="flex flex-col gap-y-2">
+							{/* Bio  */}
+
+							<h2>Christian Zintel</h2>
+							<p>33 · Mediengestalter · Medienfachwirt · Frontend Developer</p>
+							<p>
+								Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+								diam nonumy eirmod tempor invidunt ut labore et dolore magna
+								aliquyam erat, sed diam voluptua. At vero eos et accusam et
+								justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+								takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+								dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+								eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+								sed diam voluptua.
+							</p>
+						</div>
+
+						{/* Stack & Skills  */}
+
+						<div className="flex flex-col gap-y-2">
+							<h3>Stack & Skills</h3>
+
+							<div>
+								<div>Techstack</div>
+								<p>
+									Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+									diam nonumy eirmod tempor invidunt ut labore et dolore magna
+									aliquyam erat.
+								</p>
+							</div>
+
+							<div>
+								<div>Software</div>
+								<p>
+									Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+									diam nonumy eirmod tempor invidunt ut labore et dolore magna
+									aliquyam erat.
+								</p>
+							</div>
+						</div>
+
+						{/* Qualifikationen */}
+
+						<div className="flex flex-col gap-y-2">
+							<h3>Qualifikationen und Weiterbildungen</h3>
+
+							<div>
+								<div>Ausbildung zum Mediengestalter</div>
+								<p>
+									Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+									diam nonumy eirmod tempor invidunt ut labore et dolore magna
+									aliquyam erat.
+								</p>
+							</div>
+
+							<div>
+								<div>berufsbegleitende Weiterbildung zum Medienfachwirt</div>
+								<p>
+									Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+									diam nonumy eirmod tempor invidunt ut labore et dolore magna
+									aliquyam erat.
+								</p>
+							</div>
+
+							<div>
+								<div>
+									berufsbegleitende Weiterbildung zum Softwareentwickler
+								</div>
+								<p>
+									Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+									diam nonumy eirmod tempor invidunt ut labore et dolore magna
+									aliquyam erat.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</main>
 	);
